cfd00102434ae9ce4676f4907c8483c4c11c992c
Luna <bedgowns@gmail.com>
2021-07-05 00:30:59 -0700

    transaction module

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,27 @@ import { application } from "express"
 import { TOKENS } from "./di"
 import { container } from "tsyringe"
 import { RobloxExpressComponent } from "@components/roblox/express"
-import { PostgresModule } from "@modules/postgres/pg"
-import appConfig from "@config/"
+import { TransactionDBModule } from "@modules/transaction"
+import { Uuid } from "@common/uuid"
+import { v4} from "uuid"
+import { DiscordId } from "@common/discordId"
+import { Username } from "@common/username"
+import { Id } from "@common/id"
 
 const app = container.resolve<typeof application>(TOKENS.values.expressApp)
 
 const component = container.resolve<RobloxExpressComponent>(TOKENS.components.roblox.component)
 component.execute()
 
-const postgres = container.resolve<PostgresModule>(TOKENS.modules.postgres)
+const transactionDB = container.resolve<TransactionDBModule>(TOKENS.modules.transactionDb)
 
 async function test() {
-    const client = await postgres.getPGClient({
-        host : appConfig.postgres.host,
-        port : parseInt(appConfig.postgres.port),
-        user : appConfig.postgres.user,
-        password : appConfig.postgres.password,
-        database : appConfig.postgres.database
-    })
-
-    console.log(client)
-
+ 
+    console.log(a)
 }
 
 test()
 
 // app.listen(3000 , () => console.log('Live on PORT 3000'))
+
+// id uuid NOT NULL
