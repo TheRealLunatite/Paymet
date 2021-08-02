57f2ca39e7c1f7fa19ef673d0cfd5dc62f2f64c2
Luna <bedgowns@gmail.com>
2021-08-02 00:37:23 -0700

    i don't know what the fuck i did

--- src/components/transaction/express/routes/create/index.ts
+++ src/components/transaction/express/routes/create/index.ts
@@ -1,44 +1,38 @@
 import { Router } from "express";
-import { IExpressRoute } from "@common/interfaces/IExpressRoute";
-import { TransactionModule } from "@modules/transaction/types";
 import { autoInjectable, inject } from "tsyringe";
 import { TOKENS } from "src/di";
 import { v4 as uuid } from "uuid";
-import CreateTransactionValidationMiddleware from "../../middleware/createTransactionValidation";
-import { CreateTransactionRequestValidatedBody } from "./types";
+
+import { IExpressRoute } from "@common/interfaces/IExpressRoute";
 import { Uuid } from "@common/uuid";
+
+import { TransactionModule } from "@modules/transaction/types";
 import { IRobloxModule } from "@modules/roblox/types";
-import { PriceModule } from "@modules/prices/types";
+
+import CreateTransactionValidationMiddleware from "../../middleware/createTransactionValidation";
+import ValidateItemsMiddleware from "../../middleware/validateItems"
+import GetTotalPriceMiddleware from "../../middleware/getTotalPrice"
+
 
 @autoInjectable()
 export class CreateTransactionRoute implements IExpressRoute {
     constructor(
         @inject(TOKENS.modules.transactionDb) private transactionDb? : TransactionModule,
         @inject(TOKENS.modules.roblox) private roblox? : IRobloxModule,
-        @inject(TOKENS.modules.priceDb) private priceDb? : PriceModule,
         @inject(TOKENS.values.uuid) private v4? : typeof uuid
     ) {}
 
     execute(router : Router) : void {
-        router.post('/create' , CreateTransactionValidationMiddleware.value , async (req , res , next) => {
-            const { username  , discordId , items } : CreateTransactionRequestValidatedBody = req.body
-            
+        router.post('/create' , CreateTransactionValidationMiddleware.value , ValidateItemsMiddleware.value , GetTotalPriceMiddleware.value , async (req , res , next) => {
             try {
-                const { id } = await this.transactionDb!.add({
-                    id : new Uuid(this.v4!()),
-                    status : "initalized",
-                    username,
-                    discordId, 
-                    items
-                })
 
-                return res.status(200).json({
-                    id : id.value
-                })
 
-            } catch {
-                next(new Error("There was a problem creating a new transaction."))
+
+            } catch (e) {
+                return next(e)
             }
+
+
         })
     }
 }
